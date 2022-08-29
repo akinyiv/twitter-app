0665769b520b979ed5f24d14813fa2b9e01ea838
akinyiv <akinyivee10@gmail.com>
2022-08-29 12:57:24 +0300

    user

--- backend/models/user.js
+++ backend/models/user.js
@@ -13,6 +13,32 @@ const userSchema = new mongoose.Schema({
     },
     profileImageUrl: {
         type: String,
+    },
+});
+
+userSchema.pre("save", async function(next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch(err) {
+        return next(err);
     }
-})
+});
+
+userSchema.methods.comparePassword = async function(typedPassword, next) {
+    try {
+        let isMatch = await bcrypt.compare(typedPassword, this.password);
+        return isMatch;
+    } catch (err) {
+        return next(err);
+    }
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
 
